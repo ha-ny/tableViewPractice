c109535337ca75c53a9ead5478ff3bdcee6c0d9a
ha-ny <hany01.dev@gmail.com>
2023-07-27 18:06:00 +0900

    addList -> custom으로 변경을 안하니까 오른쪽 이미지를 어떻게 넣는지 모르겠음..

---
+++ AddListTableViewController.swift
@@ -0,0 +1,40 @@
+//
+//  AddListTableViewController.swift
+//  tableViewPractice
+//
+//  Created by 김하은 on 2023/07/27.
+//
+
+import UIKit
+
+class AddListTableViewController: UITableViewController {
+
+    var list = [String]()
+    
+    @IBOutlet var listTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.rowHeight = 60
+    }
+
+    @IBAction func addButtonClick(_ sender: UIButton) {
+        
+        if let text = listTextField.text{
+            list.append(text)
+        }
+        tableView.reloadData()
+    }
+    // MARK: - Table view data source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return list.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "listCell")!
+        cell.textLabel?.text = list[indexPath.row]
+        
+        return cell
+    }
+}
